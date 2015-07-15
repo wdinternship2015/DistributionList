e8eff780bca695e28ac5a4248926481397b990a4
elisa.yan <elisa.yan@workday.com>
2015-07-15 16:23:49 -0700

    changed main.js to mainCtrl.js

--- DistrListAngularJS/app/js/PageCtrlView3.js
+++ DistrListAngularJS/app/js/PageCtrlView3.js
@@ -1,5 +1,19 @@
-angular.module('view3Module', []).controller('PageCtrlView3', function ($scope, shareDataService/* $location, $http */) {
+angular.module('view3Module', ['checklist-model']).controller('PageCtrlView3', function ($scope, shareDataService/* $location, $http */) {
 	  console.log("PageCtrlView3 reporting for duty.");
 	  $scope.workers = shareDataService.get();
 	  $scope.workersize = $scope.workers.length;
+	  
+	  $scope.deleteWorker = {
+			    workers: []
+			  };
+			  $scope.checkAll = function() {
+			    $scope.user.roles = angular.copy($scope.roles);
+			  };
+			  $scope.uncheckAll = function() {
+			    $scope.user.roles = [];
+			  };
+			  $scope.checkFirst = function() {
+			    $scope.user.roles.splice(0, $scope.user.roles.length); 
+			    $scope.user.roles.push($scope.roles[0]);
+			  };
 });
