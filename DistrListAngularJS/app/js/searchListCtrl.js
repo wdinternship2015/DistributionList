cab3d52efe70e1737f99afe310c293afa80c484d
elisa.yan <elisa.yan@workday.com>
2015-08-04 13:02:07 -0700

    search works

--- DistrListAngularJS/app/js/searchListCtrl.js
+++ DistrListAngularJS/app/js/searchListCtrl.js
@@ -1,4 +1,4 @@
-angular.module('searchListModule').controller('searchListCtrl', function ($scope, shareDataService, $log, $localStorage) {
+angular.module('searchListModule').controller('searchListCtrl', function ($scope, shareDataService,requestService, $log, $localStorage) {
 	 // console.log("searchListCtrl reporting for duty.");
 	  console.log('searchListCtrl token: ' + $scope.token); 
 	  //$scope.$storage.token
@@ -14,18 +14,20 @@ angular.module('searchListModule').controller('searchListCtrl', function ($scope
 			groups :[]
 		};
 
-		//make REST call here to get search result
-		$scope.search = function(searchParam)  {
-			
-			var obj = JSON.parse(sample);
-			
-			
-			$scope.searchResultGroups = obj.data;
-	//		$scope.searchResultGroups = $scope.resultGroups;
+		//make REST call here to get search result, still using get all lists
+		$scope.search = function(searchParam) {
+			requestService.getDistrLists($scope.token).then(
+					function(success) {
+						var obj = success.data;
+						$scope.searchResultGroups = obj.data;
+					}, 
+				      function(error){
+				        
+				    }
+			);
 		};
 
 });
-
 
 angular.module('searchListModule').filter('privacy', function() {
     return function(input) {
@@ -33,5 +35,6 @@ angular.module('searchListModule').filter('privacy', function() {
     }
 });
 
-var sample = '{"total":2,"data":[{"descriptor":"","id":"22376a6ad62510000c14dd89deea0028","private":false,"iid":"16237$4"},{"descriptor":"The First List","id":"7c3331224db21000058faf4b9049001b","lastUpdateBy":{"descriptor":"Isla Koenig","id":"fd30c20b3d4a4801a5c4a272327e77fd"},"description":"A test list","organization":{"descriptor":"500.1 Global Modern Services, Inc. (USA)","id":"cb550da820584750aae8f807882fa79a"},"private":false,"name":"The First List","alias":"list1","iid":"16237$1"}]}';
-var testSearchGroups = [{"id": "008", "name": "search name", "alias": "search@alias", "description": "test search description", "visibility": "Public", "members": [{"name":"Alice", "org": "WD Alice"}, {"name":"Bob", "org": "WD Bob"}, {"name":"Charlie", "org": "WD Charlie"}, {"name":"Dilbert", "org": "WD Dilbert"}]}, {"id": "009", "name": "search name 2", "alias": "search2@alias", "description": "test search 2 description", "visibility": "Private", "members": [{"name":"Elle", "org": "WD Elle"}, {"name":"Frank", "org": "WD Frank"}, {"name":"Gary", "org": "WD Gary"}, {"name":"Henry", "org": "WD Henry"}]}];	  
+
+
+
--- DistrListAngularJS/app/services/requestService.js
+++ DistrListAngularJS/app/services/requestService.js
@@ -4,8 +4,9 @@ angular.module('DistributionList').factory('requestService',['$http', function($
     var response = 
       $http({
         url: "https://i-7ad0de8d.workdaysuv.com/ccx/internalapi/emailDList/v1/super/distributionList",
-        method: 'POST',
-        headers: {'Content Type' : 'application/json'}, 
+        method: 'GET',
+        //headers: {'Content Type' : 'application/json'}, 
+        headers: {'Authorization': 'Bearer ' + token},
         }).success(function(data, status, headers, config){
         return data;
       }).error(function(data, status, headers, config){
